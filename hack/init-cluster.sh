20a27906e692e70ffdedbceb47a86d8daa456857
jmcshane <james.mcshane@superorbital.io>
2024-03-30 21:00:46 -0400

    Preparing cluster

--- hack/init-cluster.sh
+++ hack/init-cluster.sh
@@ -10,6 +10,9 @@ kubectl rollout -n argocd status deployment argocd-server
 
 clusterctl init --infrastructure vcluster
 
+kubectl rollout -n capi-system status deployment argocd-server
+
+sleep 5
 (
   export CLUSTER_NAME=kind
   export CLUSTER_NAMESPACE=vcluster
@@ -22,3 +25,16 @@ clusterctl init --infrastructure vcluster
       --kubernetes-version ${KUBERNETES_VERSION} \
       --target-namespace ${CLUSTER_NAMESPACE} | kubectl apply -f -
 )
+
+if [ "${FULL_TEST_RUN}" = "true" ]; then
+
+  ARGOCD_PASS=$(kubectl get secret -n argocd argocd-initial-admin-secret -o template --template {{.data.password}} | base64 --decode)
+
+  argocd login --username admin --password "${ARGOCD_PASS}" --port-forward-namespace argocd --port-forward
+
+  argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-namespace default --dest-name vcluster-kind --sync-policy=automated
+
+  vcluster connect kind -n vcluster
+  kubectl rollout status deployment guestbook-ui
+  vcluster disconnect
+fi
