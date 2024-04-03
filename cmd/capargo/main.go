5460d6c7ea1b2f27af2aeec2f973ceed875182a2
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2024-04-03 09:49:27 -0400

    Add taskfile for building capargo binary

--- cmd/capargo/main.go
+++ cmd/capargo/main.go
@@ -8,26 +8,39 @@ import (
 	"github.com/superorbital/capargo/internal/controller"
 	"istio.io/istio/pkg/cluster"
 	"istio.io/istio/pkg/kube"
-	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	restconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
-var clientset *kubernetes.Clientset
-var client kube.Client
+var (
+	Version   = devVersion
+	BuildTime = unknown
+	Revision  = unknown
+)
 
-func init() {
-	var err error
+const unknown = "unknown"
+const devVersion = "dev"
+
+type BuildInfo struct {
+	Version   string
+	GitCommit string
+	BuildTime string
+}
+
+func main() {
+	b := BuildInfo{
+		BuildTime: BuildTime,
+		GitCommit: Revision,
+		Version:   Version,
+	}
+	fmt.Printf("Starting up capargo binary\n%+v\n", b)
 	config := restconfig.GetConfigOrDie()
 	clientcfg := kube.NewClientConfigForRestConfig(config)
-	client, err = kube.NewClient(clientcfg, cluster.ID("kind-kind"))
+	client, err := kube.NewClient(clientcfg, cluster.ID("kind-kind"))
 	if err != nil {
 		fmt.Printf("Unable to initialize Kubernetes client: %s\n", err)
 		os.Exit(1)
 	}
-}
-
-func main() {
 	ctx := ctrl.SetupSignalHandler()
 	coll := controller.NewCollection(client)
 	go coll.Synced().WaitUntilSynced(ctx.Done())
