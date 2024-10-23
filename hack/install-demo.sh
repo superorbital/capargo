6df7a44850c1f9599ab0280add426b1ee2e7e40a
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2024-10-23 16:10:57 -0400

    Fix vcluster demo not installing due to malformed helm values
    Helm values changed significantly after chart version 0.19.7. More work is needed to update
    the values for 0.20.0+ helm charts

--- hack/install-demo.sh
+++ hack/install-demo.sh
@@ -17,14 +17,53 @@ kubectl --kubeconfig "${kubeconfig_file}" apply -k manifests/kustomize
 kubectl --kubeconfig "${kubeconfig_file}" rollout --namespace capargo status deployment capargo
 
 # Create vcluster cluster
-kubectl --kubeconfig "${kubeconfig_file}" create namespace vcluster
+VCLUSTER_NAME=vcluster-1
+VCLUSTER_NAMESPACE=vcluster
+CHART_VERSION=0.19.7
+
+cat <<EOF | kubectl --kubeconfig "${kubeconfig_file}" apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: "${VCLUSTER_NAMESPACE}"
+EOF
 kubectl --kubeconfig "${kubeconfig_file}" wait --for jsonpath='{.status.phase}=Active' --timeout=5s namespace/vcluster
-HELM_VALUES="service:\n  type: NodePort\nsyncer:\n  extraArgs:\n  - --tls-san=vcluster-1.vcluster.svc" clusterctl generate cluster vcluster-1 \
-  --kubeconfig "${kubeconfig_file}" \
-  --infrastructure vcluster \
-  --kubernetes-version v1.29.2 \
-  --target-namespace vcluster | kubectl  --kubeconfig "${kubeconfig_file}" apply -f -
-kubectl --kubeconfig "${kubeconfig_file}" wait --for jsonpath='{.status.phase}=Provisioned' --timeout=120s --namespace vcluster cluster/vcluster-1
+
+cat <<EOF | kubectl --kubeconfig "${kubeconfig_file}" apply -f -
+apiVersion: cluster.x-k8s.io/v1beta1
+kind: Cluster
+metadata:
+  name: "${VCLUSTER_NAME}"
+  namespace: "${VCLUSTER_NAMESPACE}"
+spec:
+  infrastructureRef:
+    apiVersion: infrastructure.cluster.x-k8s.io/v1alpha1
+    kind: VCluster
+    name: "${VCLUSTER_NAME}"
+  controlPlaneRef:
+    apiVersion: infrastructure.cluster.x-k8s.io/v1alpha1
+    kind: VCluster
+    name: "${VCLUSTER_NAME}"
+---
+apiVersion: infrastructure.cluster.x-k8s.io/v1alpha1
+kind: VCluster
+metadata:
+  name: "${VCLUSTER_NAME}"
+  namespace: "${VCLUSTER_NAMESPACE}"
+spec:
+  helmRelease:
+    values: |-
+      service:
+        type: NodePort
+      syncer:
+        extraArgs:
+        - --tls-san=${VCLUSTER_NAME}.${VCLUSTER_NAMESPACE}.svc
+    chart:
+      repo: https://charts.loft.sh
+      name: vcluster
+      version: ${CHART_VERSION}
+EOF
+kubectl --kubeconfig "${kubeconfig_file}" wait --for jsonpath='{.status.phase}=Provisioned' --timeout=120s --namespace "${VCLUSTER_NAMESPACE}" cluster/"${VCLUSTER_NAME}"
 
 # Log into ArgoCD
 KUBECONFIG="${kubeconfig_file}" argocd login \
@@ -33,17 +72,17 @@ KUBECONFIG="${kubeconfig_file}" argocd login \
   --username admin \
   --password "$(kubectl --kubeconfig "${kubeconfig_file}" get secret -n argocd argocd-initial-admin-secret -o template --template '{{.data.password}}' | base64 --decode)"
 
-# Create test Application for vcluster-1
+# Create test Application for vcluster
 KUBECONFIG="${kubeconfig_file}" argocd app create guestbook \
   --port-forward-namespace argocd \
   --port-forward \
   --repo https://github.com/argoproj/argocd-example-apps.git \
   --path guestbook \
   --dest-namespace default \
-  --dest-name vcluster-1 \
+  --dest-name "${VCLUSTER_NAME}" \
   --sync-policy=automated
 
-# Wait until vcluster-1 has the Application deployed to it
-KUBECONFIG="${kubeconfig_file}" vcluster connect vcluster-1 --namespace vcluster
+# Wait until vcluster has the Application deployed to it
+KUBECONFIG="${kubeconfig_file}" vcluster connect "${VCLUSTER_NAME}" --namespace "${VCLUSTER_NAMESPACE}"
 KUBECONFIG="${kubeconfig_file}" kubectl rollout status deployment guestbook-ui
 KUBECONFIG="${kubeconfig_file}" vcluster disconnect
